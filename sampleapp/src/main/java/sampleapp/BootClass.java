57bd5941ebf98970523f5b78c83ce58206dc19dc
affu12 <afreen70133@gmail.com>
2021-09-20 10:59:54 +0530

    show method added

--- sampleapp/src/main/java/sampleapp/BootClass.java
+++ sampleapp/src/main/java/sampleapp/BootClass.java
@@ -4,7 +4,12 @@ public class BootClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello! from STS!");
+		show();
 
+	}
+	
+	public static void show() {
+		System.out.println("show method here!");
 	}
 
 }
