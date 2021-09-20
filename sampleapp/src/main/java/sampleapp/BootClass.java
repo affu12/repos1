d784c6ffa228f0b80655dd434530ed62a61adf34
affu12 <afreen70133@gmail.com>
2021-09-20 11:39:16 +0530

    Boot class added

--- sampleapp/src/main/java/sampleapp/BootClass.java
+++ sampleapp/src/main/java/sampleapp/BootClass.java
@@ -4,7 +4,16 @@ public class BootClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello! from STS!");
-
+        show();
+	}
+	public static void show() {
+		int[] arr= {1,2,3,4,5};
+		for(int i=0;i<100;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		System.out.println("Show method here!");
+		
 	}
 
 }
